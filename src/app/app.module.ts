84eb656a6e6dee69a59efca0c53b6d639753203b
Marcos Carvalho <marcosfcar@gmail.com>
2020-11-21 11:06:28 -0300

    Layouting work

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HistorySidebarComponent } from './history-sidebar/history-sidebar.component';
-import { EditorCanvasComponent } from './editor-canvas/editor-canvas.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HistorySidebarComponent} from './history-sidebar/history-sidebar.component';
+import {EditorCanvasComponent} from './editor-canvas/editor-canvas.component';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatOptionModule} from '@angular/material/core';
@@ -14,12 +14,16 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatCardModule} from '@angular/material/card';
+import {FooterComponent} from './shared/components/footer/footer.component';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     HistorySidebarComponent,
-    EditorCanvasComponent
+    EditorCanvasComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -33,9 +37,12 @@ import {MatCardModule} from '@angular/material/card';
     MatSelectModule,
     MatAutocompleteModule,
     MatSidenavModule,
-    MatCardModule
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
