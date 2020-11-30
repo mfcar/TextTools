be5b15e2da5325f4ed7b1b0d4a3fe83be010022d
texttools <marcosfcar@gmail.com>
2020-11-30 14:09:49 +0000

    Build from 2fb239671dcef0437b1c895aab65e53a87ef25d7 in the source repository.
    Triggered by commit: https://github.com/mfcar/TextTools/commit/2fb239671dcef0437b1c895aab65e53a87ef25d7

--- scripts.3fffd0e710f590e11ffe.js
+++
@@ -1,1 +0,0 @@
-!function(e,a,t,n,g,c,o){e.GoogleAnalyticsObject="ga",e.ga=e.ga||function(){(e.ga.q=e.ga.q||[]).push(arguments)},e.ga.l=1*new Date,c=a.createElement(t),o=a.getElementsByTagName(t)[0],c.async=1,c.src="https://www.google-analytics.com/analytics.js",o.parentNode.insertBefore(c,o)}(window,document,"script"),ga("create","UA-38960196-1","auto"),ga("send","pageview");
