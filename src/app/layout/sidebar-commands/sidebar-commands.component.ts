96bccbf4e0ae144f9a05e6b863cb8e39bd234b3c
Marcos Carvalho <marcosfcar@gmail.com>
2021-12-30 19:56:36 -0300

    Add lint support

--- src/app/layout/sidebar-commands/sidebar-commands.component.ts
+++ src/app/layout/sidebar-commands/sidebar-commands.component.ts
@@ -1,15 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-sidebar-commands',
   templateUrl: './sidebar-commands.component.html',
   styleUrls: ['./sidebar-commands.component.scss']
 })
-export class SidebarCommandsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-}
+export class SidebarCommandsComponent { }
